49cdc2bd00c3c2a95c2f70c4cd8eb0ea2b345200
Ceejay Gabrang <71625955+CEGA22@users.noreply.github.com>
2022-06-18 22:33:47 +0800

    modify FolderWatcher class

--- FolderWatcherService/FolderWatcher.cs
+++ FolderWatcherService/FolderWatcher.cs
@@ -6,23 +6,19 @@ using System.IO;
 using System.Threading.Tasks;
 using System.Timers;
 using System.Diagnostics;
+using System.Configuration;
 
 namespace FolderWatcherService
 {
     public class FolderWatcher
     {       
-        public string path = @"C:\Folder1";
-        EventLog eventLog = new EventLog();     
+        static string path = @"C:\Folder1";
+        static readonly EventLog eventLog = new EventLog(); 
+        
         public FolderWatcher()
-        {         
+        {          
             MonitorDirectory(path);
-        }
-
-        public void TimerElapsed(object? sender, ElapsedEventArgs e)
-        {                          
-            MonitorDirectory(path);
-        }
-
+        }       
         private static void MonitorDirectory(string path)
         {
             FileSystemWatcher fileSystemWatcher = new FileSystemWatcher();           
@@ -34,7 +30,6 @@ namespace FolderWatcherService
             fileSystemWatcher.Renamed += FileSystemWatcher_Renamed;
             fileSystemWatcher.EnableRaisingEvents = true;          
         }
-
         private static void WriteLine(string line)
         {
             try
@@ -45,18 +40,22 @@ namespace FolderWatcherService
             {
                 var x = e.Message;               
             }
-        }
-   
+        } 
         private static void FileSystemWatcher_Created(object sender, FileSystemEventArgs e)
         {
             try
             {
                 Console.WriteLine(e.ChangeType + " " + e.FullPath + " " + DateTime.Now.ToString());
-                WriteLine(e.ChangeType + " " +  e.FullPath + " " + DateTime.Now.ToString());
+                WriteLine(e.ChangeType + " | " +  e.FullPath + " | " + DateTime.Now.ToString());
+                var message = e.ChangeType + " " + e.FullPath;
+                eventLog.Source = "FolderWatcherService";
+                eventLog.WriteEntry(message, EventLogEntryType.Information);
             }
-            catch (Exception c)
+            catch (Exception)
             {
-                var x = c.Message;
+                var errorMessage = "Error occured. Please try again " + e.ChangeType + " | " + e.FullPath;
+                eventLog.Source = "FolderWatcherService";
+                eventLog.WriteEntry(errorMessage, EventLogEntryType.Warning);
             }
         }
         private static void FileSystemWatcher_Renamed(object sender, RenamedEventArgs e)
@@ -64,23 +63,28 @@ namespace FolderWatcherService
             try
             {
                 Console.WriteLine("File renamed: " + e.OldFullPath + " to " + e.FullPath + " " + DateTime.Now.ToString());
-                WriteLine("File renamed: " + e.OldFullPath + " to " + e.FullPath + " " + DateTime.Now.ToString());
+                WriteLine("File renamed: \n" + "From " + e.OldName + " to " + e.Name + " | " + DateTime.Now.ToString());
+                var message = "File renamed: " + e.ChangeType + " " + e.FullPath;
+                eventLog.Source = "FolderWatcherService";
+                eventLog.WriteEntry(message, EventLogEntryType.Information);
             }
-            catch (Exception n )
+            catch (Exception)
             {
-                var s = n.Message;
+                var errorMessage = "File renamed: " + e.ChangeType + " " + e.FullPath;
+                eventLog.Source = "FolderWatcherService";
+                eventLog.WriteEntry(errorMessage, EventLogEntryType.Warning);
             }           
         }               
         public void Start()
         {
             eventLog.Source = "FolderWatcherService";
-            eventLog.WriteEntry("This is a test message for start.", EventLogEntryType.Information);                       
+            eventLog.WriteEntry("This is a test message for start demo.", EventLogEntryType.Information);                       
         }
 
         public void Stop()
         {
             eventLog.Source = "FolderWatcherService";
-            eventLog.WriteEntry("This is a test message for stopped.", EventLogEntryType.Information);               
+            eventLog.WriteEntry("This is a test message for stopped demo.", EventLogEntryType.Information);               
         }      
     }
 }
