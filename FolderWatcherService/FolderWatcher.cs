b2d33108ca4c8a8e6537978e45cd5d4a09b27ddb
Ceejay Gabrang <71625955+CEGA22@users.noreply.github.com>
2022-06-18 18:23:44 +0800

    Added FolderWatcherService project file

---
+++ FolderWatcherService/FolderWatcher.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Diagnostics;
+
+namespace FolderWatcherService
+{
+    public class FolderWatcher
+    {       
+        public string path = @"C:\Folder1";
+        EventLog eventLog = new EventLog();     
+        public FolderWatcher()
+        {         
+            MonitorDirectory(path);
+        }
+
+        public void TimerElapsed(object? sender, ElapsedEventArgs e)
+        {                          
+            MonitorDirectory(path);
+        }
+
+        private static void MonitorDirectory(string path)
+        {
+            FileSystemWatcher fileSystemWatcher = new FileSystemWatcher();           
+            fileSystemWatcher.Path = path;
+            fileSystemWatcher.NotifyFilter = NotifyFilters.FileName| NotifyFilters.DirectoryName | NotifyFilters.Attributes;
+            fileSystemWatcher.Created += FileSystemWatcher_Created;          
+            fileSystemWatcher.Deleted += FileSystemWatcher_Created;
+            fileSystemWatcher.Changed += FileSystemWatcher_Created;
+            fileSystemWatcher.Renamed += FileSystemWatcher_Renamed;
+            fileSystemWatcher.EnableRaisingEvents = true;          
+        }
+
+        private static void WriteLine(string line)
+        {
+            try
+            {
+                File.AppendAllText(@"C:\Folder1\FolderWatch.txt", Environment.NewLine + line);
+            }
+            catch (Exception e)
+            {
+                var x = e.Message;               
+            }
+        }
+   
+        private static void FileSystemWatcher_Created(object sender, FileSystemEventArgs e)
+        {
+            try
+            {
+                Console.WriteLine(e.ChangeType + " " + e.FullPath + " " + DateTime.Now.ToString());
+                WriteLine(e.ChangeType + " " +  e.FullPath + " " + DateTime.Now.ToString());
+            }
+            catch (Exception c)
+            {
+                var x = c.Message;
+            }
+        }
+        private static void FileSystemWatcher_Renamed(object sender, RenamedEventArgs e)
+        {
+            try
+            {
+                Console.WriteLine("File renamed: " + e.OldFullPath + " to " + e.FullPath + " " + DateTime.Now.ToString());
+                WriteLine("File renamed: " + e.OldFullPath + " to " + e.FullPath + " " + DateTime.Now.ToString());
+            }
+            catch (Exception n )
+            {
+                var s = n.Message;
+            }           
+        }               
+        public void Start()
+        {
+            eventLog.Source = "FolderWatcherService";
+            eventLog.WriteEntry("This is a test message for start.", EventLogEntryType.Information);                       
+        }
+
+        public void Stop()
+        {
+            eventLog.Source = "FolderWatcherService";
+            eventLog.WriteEntry("This is a test message for stopped.", EventLogEntryType.Information);               
+        }      
+    }
+}
---
+++ FolderWatcherService/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FolderWatcherService;
+using System.Diagnostics;
+using Topshelf;
+
+var exitCode = HostFactory.Run(x =>
+{
+    x.Service<FolderWatcher>(f =>
+    {
+        f.ConstructUsing(folderwatch => new FolderWatcher());
+        f.WhenStarted(folderwatch => folderwatch.Start());
+        f.WhenStopped(folderwatch => folderwatch.Stop());
+    });
+
+    x.RunAsLocalSystem();
+    x.SetServiceName("FolderWatcherService");
+    x.SetDisplayName("Folder Watcher Service");
+    x.SetDescription("This is the sample folder watcher service used in a demo");
+});
+
+int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+Environment.Exit(exitCodeValue);
+
+
